58ce7364e01f28f47d979de3b30105e7adb1570a
Jian Lin <linjian815@gmail.com>
2012-06-04 16:17:26 +0800

    fix puts_to_log for rails console

--- code/puts_to_log.rb
+++ code/puts_to_log.rb
@@ -1,3 +1,3 @@
-# Add the following line to config/environments/development.rb
+# Add the following line to config/environments/development.rb or config/initializers/
 # So I can use puts or p to print debug message to log file when I use nginx, which has no way to see STDOUT directly.
-STDOUT.reopen(File.open("#{Rails.root}/log/development.log", 'a+'))
+STDOUT.reopen(File.open("#{Rails.root}/log/development.log", 'a+')) unless $0 =~ /script\/rails/
