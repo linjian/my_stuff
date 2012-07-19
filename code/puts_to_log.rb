6cc98026c063cae07d452db1f60d5a9f1873e29b
Jian Lin <linjian815@gmail.com>
2012-07-19 13:53:07 +0800

    clean code

--- code/puts_to_log.rb
+++
@@ -1,5 +0,0 @@
-# Add the following line to config/environments/development.rb or config/initializers/
-# So I can use puts or p to print debug message to log file when I use nginx, which has no way to see STDOUT directly.
-if Rails.env.development? && $0 !~ /script\/rails/ && $0 !~ /rake/
-  STDOUT.reopen(File.open("#{Rails.root}/log/development.log", 'a+'))
-end
